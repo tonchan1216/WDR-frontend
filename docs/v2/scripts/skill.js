cc78311a8c3af129afaf732954119668491538a2
tonchan <cleartone1216@gmail.com>
2019-09-26 23:23:46 +0900

    add v2

---
+++ docs/v2/scripts/skill.js
@@ -0,0 +1,19 @@
+export function addSkill () {
+  let skillBox = document.getElementById('skill-box');
+  const skillName = skillBox.value;
+  let article = document.createElement('article');  // article要素を生成
+  let h3 = document.createElement('h3');  // h3要素を生成
+  let p = document.createElement('p');  // p要素を生成
+
+  if (skillName == "") return
+
+  skillBox.value = "";
+  article.className = 'skill';
+  h3.textContent = skillName;
+
+  article.appendChild(h3);
+  article.appendChild(p);
+
+  // 生成したdiv要素を追加する
+  document.getElementById('contents').appendChild(article);
+}
