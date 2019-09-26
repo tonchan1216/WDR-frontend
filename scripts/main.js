3d2099026765eca58e8413e0a93d16c0534039b3
tonchan1216 <cleartone1216@gmail.com>
2019-09-26 13:46:16 +0900

    Learn DOM Manipulation

--- scripts/main.js
+++ scripts/main.js
@@ -2,3 +2,31 @@ function sorry() {
     var result = "Sorry, Backend Stamp Rally is under construction";
     alert(result);
 }
+
+function addSkill() {
+    var skillBox = document.getElementById('skill-box');
+    var skillName = skillBox.value;
+
+    if (skillName == "") {
+        return
+    }
+
+    skillBox.value = "";
+
+    // article要素を生成
+    var article = document.createElement('article');
+    article.className = 'skill';
+
+    // h3要素を生成
+    var h3 = document.createElement('h3');
+    h3.textContent = skillName;
+
+    // p要素を生成
+    var p = document.createElement('p');
+
+    article.appendChild(h3);
+    article.appendChild(p);
+
+    // 生成したdiv要素を追加する
+    document.getElementById('contents').appendChild(article);
+}
