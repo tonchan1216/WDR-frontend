d8a364bb959a80927d8a05a5cc1cf48f21756bde
tonchan <cleartone1216@gmail.com>
2019-09-29 00:03:28 +0900

    release v6

---
+++ docs/v6/scripts/main.js
@@ -0,0 +1,16 @@
+import { fetchApi } from "./fetch.js";
+import { addSkill } from "./skill.js";
+
+document.querySelectorAll(".top-bar__link").forEach(link => {
+    link.addEventListener("click", event => {
+        event.preventDefault();
+        alert("Sorry, under construction");
+    }, false);
+});
+
+document.querySelector(".add-skill__button").addEventListener("click", event => {
+    event.preventDefault();
+    addSkill();
+}, false);
+
+fetchApi();
