7604787481e2cb9adae48d7f8fb3ad4e9dcd98bc
tonchan <cleartone1216@gmail.com>
2019-09-29 10:13:49 +0900

    Linter and Formatter

--- scripts/fetch.js
+++ scripts/fetch.js
@@ -1,10 +1,11 @@
-export function fetchApi () {
-  const url = "assets/tenki.json";
+export function fetchApi() {
+  const url = 'assets/tenki.json';
   fetch(url)
-  .then(response => response.text())
-  .then(text => {
-    const json = JSON.parse(text);
-    const json_forecasts = json["forecasts"][0];
-    document.querySelector('.news__contents').textContent = json_forecasts["date"]+"："+json_forecasts["telop"];
-  });
+    .then(response => response.text())
+    .then(text => {
+      const json = JSON.parse(text);
+      const json_forecasts = json['forecasts'][0];
+      document.querySelector('.news__contents').textContent =
+        json_forecasts['date'] + '：' + json_forecasts['telop'];
+    });
 }
--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,24 @@
-import { fetchApi } from "./fetch.js";
-import { addSkill } from "./skill.js";
+import { fetchApi } from './fetch.js';
+import { addSkill } from './skill.js';
 
-document.querySelectorAll(".top-bar__link").forEach(link => {
-    link.addEventListener("click", event => {
-        event.preventDefault();
-        alert("Sorry, under construction");
-    }, false);
+document.querySelectorAll('.top-bar__link').forEach(link => {
+  link.addEventListener(
+    'click',
+    event => {
+      event.preventDefault();
+      alert('Sorry, under construction');
+    },
+    false
+  );
 });
 
-document.querySelector(".add-skill__button").addEventListener("click", event => {
+document.querySelector('.add-skill__button').addEventListener(
+  'click',
+  event => {
     event.preventDefault();
     addSkill();
-}, false);
+  },
+  false
+);
 
 fetchApi();
