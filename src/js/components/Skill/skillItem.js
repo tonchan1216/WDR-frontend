7c53723dae11e1c6da030c7d08d0a681ebe2e83b
tonchan <cleartone1216@gmail.com>
2019-10-05 09:19:29 +0900

    syled components

--- src/js/components/Skill/skillItem.js
+++ src/js/components/Skill/skillItem.js
@@ -1,14 +1,33 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const SkillItem = ({ name }) => (
-  <article className="skill__item col-lg-3 col-md-4 col-sm-6">
-    <h3 className="skill__name">{name}</h3>
-    <p className="skill__stamp">
+  <Article className="skill__item col-lg-3 col-md-4 col-sm-6">
+    <Name className="skill__name">{name}</Name>
+    <Stamp className="skill__stamp">
       <a href="https://developer.mozilla.org/ja/docs/Learn/HTML/Introduction_to_HTML/Getting_started">
         STAMP
       </a>
-    </p>
-  </article>
+    </Stamp>
+  </Article>
 );
+
+const Article = styled.article`
+  padding: 10px;
+  background: #d9ffff;
+  flex-direction: column;
+  justify-content: space-around;
+  :nth-child(even) {
+    background: #00cccc;
+  }
+`;
+
+const Name = styled.h3`
+  text-align: center;
+`;
+
+const Stamp = styled.h3`
+  text-align: center;
+`;
 
 export default SkillItem;
--- src/js/components/footer.js
+++ src/js/components/footer.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const Footer = () => (
-  <footer className="footer">
-    <p className="footer__patent">(c) 2019 tonchan1216</p>
-  </footer>
+  <Foot className="footer">
+    <Patent className="footer__patent">(c) 2019 tonchan1216</Patent>
+  </Foot>
 );
+
+const Foot = styled.footer`
+  background-color: #ffffd9;
+  margin-right: -15px;
+  margin-left: -15px;
+`;
+
+const Patent = styled.p`
+  text-align: center;
+  padding: 20px;
+`;
 
 export default Footer;
